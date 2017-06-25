6e7cb322c32a063aec694da91f561ff68c70dc22
lprimak <lenny@flowlogix.com>
2017-06-25 15:39:23 -0500

    Fixed regression: if Java system property com.sun.corba.ee.ORBServerHost is specified, thus binding ORB to single host, do not do the multi-homed reverse IP processing

--- orbmain/src/main/java/com/sun/corba/ee/impl/resolver/BootstrapResolverImpl.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/resolver/BootstrapResolverImpl.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2016] [Payara Foundation and/or its affiliates]
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]
 
 package com.sun.corba.ee.impl.resolver ;
 
@@ -74,7 +74,7 @@ public class BootstrapResolverImpl implements Resolver {
         ObjectKey okey = new ObjectKeyImpl(orb.getWireObjectKeyTemplate(),
                                            new ObjectIdImpl(initialKey));
 
-        IIOPAddress addr = IIOPFactories.makeIIOPAddressLocalServer(host, port ) ;
+        IIOPAddress addr = IIOPFactories.makeIIOPAddressLocalServer(orb, host, port) ;
         IIOPProfileTemplate ptemp = IIOPFactories.makeIIOPProfileTemplate(
             orb, GIOPVersion.V1_0, addr);
             
