1d4d9504a71557914e228536afd2b0198b123fc9
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-04-01 17:55:05 +0530

    PAYARA-3484-JDK11 org.omg.CORBA.ORB create impl with context classloader
     Conflicts:
    	omgapi/src/main/java/org/omg/CORBA/ORB.java

--- omgapi/src/main/java/org/omg/CORBA/ORB.java
+++ omgapi/src/main/java/org/omg/CORBA/ORB.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019 Payara Foundation and/or its affiliates.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -282,7 +283,7 @@ abstract public class ORB {
             if (className == null)
                 className = defaultORBSingleton;
 
-            singleton = create_impl_with_systemclassloader(className);
+            singleton = create_impl(className);
         }
         return singleton;
     }
